ae0d741981a4b47a6cd4185b11fa5dc8fc845b3e
Emran Yasser Moustafa <s2091964@ed.ac.uk>
2025-12-13 13:18:04 +0000

    Experiments actually running well wtf

--- igp2/planlibrary/macro_action.py
+++ igp2/planlibrary/macro_action.py
@@ -612,6 +612,7 @@ class Exit(MacroAction):
         current_distance = current_lane.distance_at(state.position)
 
         frame = self.start_frame
+        # logger.debug(f"Start Point) {state.position}")
 
         connecting_lane = current_lane
         if not in_junction:
@@ -624,6 +625,7 @@ class Exit(MacroAction):
                     "termination_point": lane_follow_termination,
                     "fps": self.config.fps
                 }
+                # logger.debug(f"FollowLane termination_point) {lane_follow_termination}")
                 config = ManeuverConfig(config_dict)
                 if self.open_loop:
                     man = FollowLane(config, self.agent_id, frame, self.scenario_map)
@@ -643,6 +645,7 @@ class Exit(MacroAction):
                 "junction_lane_id": connecting_lane.id,
                 "fps": self.config.fps
             }
+            # logger.debug(f"GiveWay termination_point) {np.array(current_lane.midline.coords[-1])}")
 
             config = ManeuverConfig(config_dict)
             if self.open_loop:
@@ -680,6 +683,7 @@ class Exit(MacroAction):
             "junction_lane_id": connecting_lane.id,
             "fps": self.config.fps
         }
+        # logger.debug(f"Turn termination_point) {self.turn_target}")
         config = ManeuverConfig(config_dict)
         if self.open_loop:
             man = Turn(config, self.agent_id, frame, self.scenario_map)
@@ -816,14 +820,24 @@ class Exit(MacroAction):
         if junction:
             if not current_lane.link.predecessor is None and len(current_lane.link.predecessor) == 1:
                 connecting_lanes = [suc for suc in current_lane.link.predecessor[0].link.successor
-                                    if suc.boundary.distance(Point(state.position)) < Map.ROAD_PRECISION_ERROR] \
-                                    if not current_lane.link.predecessor[0].link.successor is None else [] #\
+                                    if suc.boundary.distance(Point(state.position)) < Map.ROAD_PRECISION_ERROR]# \
+                                    # if not current_lane.link.predecessor[0].link.successor is None else [] #\
                                     # [suc for suc in scenario_map.lanes_at(state.position, max_distance=1.5)]
+                # if current_lane.link.predecessor[0].link.successor is None:
+                #     logger.debug(f"\n\n\n\n\n\n\n\n TRIGGERED")
+                #     logger.debug(f"current_lane.link.successor, connecting_lanes = {current_lane.link.successor, connecting_lanes}")
             else:
                 raise RuntimeError(f"Junction road {current_lane.parent_road.id} had "
                 f"zero or more than one predecessor road.")
 
+        # if connecting_lanes is None or len(connecting_lanes) < 1:
+        #     logger.debug(f"Connecting lanes is empty) current_lane = {current_lane}, current_lane.link.successor = {current_lane.link.successor}, predecessor_successor = {current_lane.link.predecessor[0].link.successor}, connecting_lanes = {connecting_lanes}")
+        #     logger.debug(f"current_lane.link.predecessor, pred, succs: {current_lane.link.predecessor, current_lane.link.predecessor[0].link.predecessor, current_lane.link.predecessor[0].link.successor}")
+        #     logger.debug(f"succesor distances: {[suc.boundary.distance(Point(state.position)) for suc in current_lane.link.predecessor[0].link.successor]}")
+        if connecting_lanes is None or len(connecting_lanes) == 0:
+            connecting_lanes = [current_lane]
         for connecting_lane in connecting_lanes:
+            # logger.debug(f"not scenario_map.road_in_roundabout(connecting_lane.parent_road), len(connecting_lanes) == 1 = {not scenario_map.road_in_roundabout(connecting_lane.parent_road), len(connecting_lanes) == 1}")
             if not scenario_map.road_in_roundabout(connecting_lane.parent_road) or len(connecting_lanes) == 1:
                 targets.append(np.array(connecting_lane.midline.coords[-1]))
 
